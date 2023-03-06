f29b987d20a1b88e6642d792e5d18a6756f6da35
Mamikon Bagdasaryan <mamikonbagdasaryan@mail.ru>
2023-03-06 21:29:54 +0500

    Add. Continuous speech recognition.

--- RealTimeSpeechRecognizer/AudioStreamReceiver/BaseAudioStreamReceiver.py
+++ RealTimeSpeechRecognizer/AudioStreamReceiver/BaseAudioStreamReceiver.py
@@ -1,8 +1,13 @@
+import time
+
 import pyaudio
 import pyogg
 import MTest
+import json
 
 # pyogg library -> https://github.com/TeamPyOgg/PyOgg/tree/master/pyogg
+
+countOfFiles = 0
 
 class BaseAudioStreamReceiver:
     sound = True
@@ -11,39 +16,51 @@ class BaseAudioStreamReceiver:
     CHANNELS = 2
     RATE = 44100
     # RECORD_SECONDS = 1000
-    p = pyaudio.PyAudio()
+    # pAudio = pyaudio.PyAudio()
 
     stereoMixerName = 'Stereo Mix (Realtek(R) Audio)'
     frames = []
 
     indexOfStereoMixer = 2
 
-    stream = p.open(
-            format=FORMAT,
-            channels=CHANNELS,
-            rate=RATE,
-            input=True,
-            input_device_index=indexOfStereoMixer,
-            frames_per_buffer=CHUNK
-        )
+    # stream = pAudio.open(
+    #         format=FORMAT,
+    #         channels=CHANNELS,
+    #         rate=RATE,
+    #         input=True,
+    #         input_device_index=indexOfStereoMixer,
+    #         frames_per_buffer=CHUNK
+    #     )
 
-    def _clearStream(self):
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
+    def _clearStream(self, stream, pAudio):
+        stream.stop_stream()
+        stream.close()
+        pAudio.terminate()
 
-    def foo(self):
-        for i in range(200):
-            data = self.stream.read(self.CHUNK)
+    def readAudioStream(self, stream):
+        for i in range(200): # 4 seconds
+            data = stream.read(self.CHUNK)
             self.frames.append(data)
-            # if(self.ind == 3): break
 
     def encodeToOpus(self):
-        self.foo()
-        self._clearStream()
-        self.p.terminate()
+        fileName = time.time()
+        self.frames = []
 
-        filename = "result"
+        pAudio = pyaudio.PyAudio()
+        stream = pAudio.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            input_device_index=self.indexOfStereoMixer,
+            frames_per_buffer=self.CHUNK
+        )
+
+        self.readAudioStream(stream)
+        self._clearStream(stream, pAudio)
+        pAudio.terminate()
+
+        # filename = "result"
 
         # Create a OpusBufferedEncoder
         opus_buffered_encoder = pyogg.OpusBufferedEncoder()
@@ -53,8 +70,8 @@ class BaseAudioStreamReceiver:
         opus_buffered_encoder.set_frame_size(20)  # milliseconds
 
         # Create an OggOpusWriter
-        output_filename = filename + ".ogg"
-        print("Writing OggOpus file to '{:s}'".format(output_filename))
+        output_filename = "%s"%fileName + ".ogg"
+        # print("Writing OggOpus file to '{:s}'".format(output_filename))
         ogg_opus_writer = pyogg.OggOpusWriter(
             output_filename,
             opus_buffered_encoder
@@ -62,12 +79,30 @@ class BaseAudioStreamReceiver:
 
         # Encode the PCM data
         ogg_opus_writer.write(
-            memoryview(bytearray(b''.join(self.frames)))  # FIXME
+            memoryview(bytearray(b''.join(self.frames)))
         )
+
+        # opus_buffered_encoder = None
 
         # We've finished writing the file
         ogg_opus_writer.close()
 
-        MTest.mFoo()
+        impl = SpeechToTextImpl()
+        MTest.MTest().mFoo("%s" % fileName, impl)
 
-BaseAudioStreamReceiver().encodeToOpus()
+        self.encodeToOpus()
+
+
+class SpeechToTextImpl(MTest.SpeechToTextListener):
+    audioStreamObject = BaseAudioStreamReceiver()
+
+    def doAfterTextRecognition(self, recognizedText):
+        recognizedObject = json.loads(recognizedText)
+        print("Result: ", recognizedObject["result"])
+        basr = BaseAudioStreamReceiver()
+        basr.encodeToOpus()
+
+
+if __name__ == "__main__":
+    basr = BaseAudioStreamReceiver()
+    basr.encodeToOpus()
